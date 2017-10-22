3e7c8e92dcd54b932107dd12991132469b10730f
Eryk Szugzda <eszugzda@linux-p110-9.inf.ug.edu.pl>
2017-10-22 11:57:19 +0200

    wrzuciłem plik z wyrażeniami arytm. zajecia2-1.c

---
+++ zajecia2-1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  printf("%d\n", 2+2 ); //dodawanie
+  printf("%d\n", 2-2 ); //odejmowanie
+  printf("%d\n", 2*2 ); //mnożenie
+  printf("%d\n", 2/2 ); //dzielenie
+  printf("%d\n", 2>=2); //większe bądź równe
+  printf("%d\n", 2>2 ); //wieksze
+  printf("%d\n", 2<2 ); //mniejsze
+  printf("%d\n", 2<=2); //mniejsze bądź równe
+  printf("%d\n", 2==2); //równe
+  printf("%d\n", 2!=2); //różne
+  printf("%d\n", 2>>2); //przesunięcie bitowe
+  printf("%d\n", 2<<2); //przesuniecie bitowe
+  printf("%d\n", 2&&2); //koniunkcja
+  printf("%d\n", 2||2); //alternatywa
+  printf("%d\n", 2&2 ); //koniunkcja bitowa
+  printf("%d\n", 2|2 ); //alternatywa bitowa
+  return 0;
+}
